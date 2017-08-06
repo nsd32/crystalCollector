55ab77262b9b40980717253c8b22ea994c20fc6f
Lea Abetya <leaabetya@Leas-MacBook-Air-2.local>
2017-08-06 14:15:38 -0600

    wins and losses working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,69 @@
-var gameNumber = Math.floor(Math.random() * 120) + 19;
 
-var gameNumberHtml = $('#game-number');
+var gameNumber = Math.floor(Math.random() * 120) + 19;
 
 var imageButtons = $('.crystal-button');
 
-var greenImage = $('#green-image');
-var blueImage = $('#blue-image');
-var purpleImage = $('#purple-image');
-var orangeImage = $('#orange-image');
-var playerScore = $('#player-score')
-
-var greenImageNumber;
-var blueImageNumber;
-var purpleImageNumber;
-var orangeImageNumber;
-
 var score = 0;
-var wins = $('#wins');
-var losses = $('#losses');
+var wins = 0;
+var losses = 0;
 
-$('#orange-image').val(orangeImageNumber); 
-console.log('this is ' + $('#orange-image').val());
+$('#game-number').html(gameNumber);
+console.log('This is gameNumber ' + gameNumber);
 
-gameNumberHtml.html(gameNumber);
-console.log(gameNumber);
+function getGameNumber() {
+  return Math.floor(Math.random() * 120) + 19;
+}
 
 function getRandomImageNumber() {
-  greenImageNumber = Math.floor(Math.random() * 12) + 1;
+  var greenImageNumber = Math.floor(Math.random() * 12) + 1;
   $('#green-image').val(greenImageNumber);
   console.log('green = ' + greenImageNumber);
   console.log('Value of green button ' + $('#green-image').val());
 
-  blueImageNumber = Math.floor(Math.random() * 12) + 1;
+  var blueImageNumber = Math.floor(Math.random() * 12) + 1;
   $('#blue-image').val(blueImageNumber);
   console.log('blue = ' + blueImageNumber);
   console.log('Value of blue button ' + $('#blue-image').val());
 
-  purpleImageNumber = Math.floor(Math.random() * 12) + 1;
+  var purpleImageNumber = Math.floor(Math.random() * 12) + 1;
   $('#purple-image').val(purpleImageNumber);
   console.log('purple = ' + purpleImageNumber);
   console.log('Value of purple button ' + $('#purple-image').val());
 
-  orangeImageNumber = Math.floor(Math.random() * 12) + 1;
+  var orangeImageNumber = Math.floor(Math.random() * 12) + 1;
   $('#orange-image').val(orangeImageNumber);
   console.log('orange = ' + orangeImageNumber);
   console.log('Value of orange button ' + $('#orange-image').val());
   
 }
 
+function resetGame() {
+  score = 0;
+
+  getRandomImageNumber();
+
+  gameNumber = getGameNumber();
+  console.log('This is new gameNumber ' + gameNumber);
+  
+  $('#game-number').html(gameNumber);
+  $('#player-score').html(score);
+}
+
 getRandomImageNumber();
 
 imageButtons.click(function() {
 	score += parseInt($(this).val());
-	playerScore.html(score);
+	$('#player-score').html(score);
+
+	if (score > gameNumber) {
+	  losses++;
+	  $('#losses').html(parseInt(losses));
+	  resetGame();
+	} else if (score === gameNumber) {
+      wins++;
+      $('#wins').html(parseInt(wins));
+      resetGame();
+	}
 })
 
 
